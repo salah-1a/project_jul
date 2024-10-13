2d6131cdb13ba92cc84ca4f315017a6e533b948b
salah-1a <young-adam@outlook.com>
2024-10-13 12:58:10 +0200

    Update settings.py

--- ai_blog_jul/mysite/mysite/settings.py
+++ ai_blog_jul/mysite/mysite/settings.py
@@ -81,14 +81,11 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'web_portfolio',
-        'USER': 'salah',
-        'PASSWORD': 'cypcac-niczy3-Kisriq',
-        'HOST': 'web-portfolio.c3s8i0m6g1od.eu-central-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
